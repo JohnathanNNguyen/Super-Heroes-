2b9e04eb7273891772aa5725ba256e7f5228c756
Johnathan Nguyen <haungocvo@gmail.com>
2021-04-29 21:12:35 -0700

    routing

--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -5,22 +5,35 @@ import Col from 'react-bootstrap/Col';
 import SuperHero from '../api/SuperHeroAPI';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { useHistory } from 'react-router-dom';
 
 const HeroList = (props) => {
+  const history = useHistory();
   const [comicsList, setComicsList] = useState([]);
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
-  useEffect(() => {});
+  // useEffect(() => {});
+
   const images = props.images.map(({ id, thumbnail, name }) => {
+    const onGetComics = () => {
+      console.log(id);
+
+      history.push('/hero/details');
+    };
+
     if (
       thumbnail.path !==
       'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available'
     ) {
       return (
         <Col key={id}>
-          <div data-aos="fade-up" className="img-container">
+          <div
+            data-aos="fade-up"
+            className="img-container"
+            onClick={onGetComics}
+          >
             <img
               className="hero-container"
               src={thumbnail.path + '.' + thumbnail.extension}
